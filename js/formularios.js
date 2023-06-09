3a76f144535aa5e1fbbb1b21b85f213620268d6f
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-06-09 11:43:34 -0600

    Changes to be committed: 	modified:   formularios.html 	new file:   js/formularios.js 	new file:   respuesta.html

---
+++ js/formularios.js
@@ -0,0 +1,22 @@
+var formulario=document.getElementById("frmlogin")
+var usuario="admin",password="0000",contador=0
+formulario.addEventListener("submit",function (evento){
+    //con la funcion prevent default se detiene  el evento 
+    evento.preventDefault()
+    var uss=document.getElementById("user").value
+    var pss=document.getElementById("pass").value
+    var btn=document.getElementById("boton")
+    var url="../respuesta.html"
+    var parametros="?usuario="+encodeURIComponent(uss)+"&contra="+encodeURIComponent(pss)
+    if(uss==usuario && pss==password)
+    {
+        window.location.href=url+parametros
+    }else{
+        if(contador<3){
+            contador=contador+1
+            alert("Credenciales incorrectas intento : "+contador)
+        }else{
+            btn.disabled=true
+        }
+    }
+})
